30fff37f79b584f788b0a4d4034f1910aa0cd990
Yamada Masaki <masaki@hisme.net>
2011-11-10 06:29:19 +0900

    install public key

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ ln -nfs $dotdir/.emacs.d $HOME/.emacs.d
 ln -nfs $dotdir/.tmux.conf $HOME/.tmux.conf
 ln -nfs $dotdir/.rvmrc $HOME/.rvmrc
 
+pubkey=`cat $dotdir/id_rsa.pub`
+authkey=$HOME/.ssh/authorized_keys
+if [ -e $authkey ]; then
+    grep "$pubkey" $authkey > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$pubkey" >> $authkey
+    fi
+else
+    echo "$pubkey" > $authkey
+fi
+
 if [ -e $HOME/Dropbox ]; then
     ln -nfs $HOME/Dropbox/.gitconfig $HOME/.gitconfig
 fi
