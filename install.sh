4b1be8b9169a3ad2b002fcd00fe54896510a896b
Yamada Masaki <masaki@hisme.net>
2012-01-29 11:02:26 +0900

    add apt-get install and rvm install and ricty install

--- install.sh
+++ install.sh
@@ -2,4 +2,15 @@
 
 echo "installing...."
 
+sudo apt-get -y install git gitk emacs lv rlwrap tmux vim curl libcurl3 chromium-browser ibus-mozc libreadline6-dev bison sqlite3 libsqlite3-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev openssh-server libssl-dev gauche yaml-mode nkf openjdk-7-jre-headless openjdk-7-jdk scala scala-library libv8-dev tk8.5 arduino
+
+if [ ! -e $HOME/.rvm ] ; then
+    curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
+fi
+
+if [ ! -e /usr/share/fonts/truetype/ttf-ricty ] ; then
+    sudo mkdir /usr/share/fonts/truetype/ttf-ricty
+fi
+sudo cp `dirname $0`/Ricty-* /usr/share/fonts/truetype/ttf-ricty
+
 sh `dirname $0`/update.sh
