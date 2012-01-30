c052123e66d119697e60e14154769b3a96231030
Yamada Masaki <masaki@hisme.net>
2012-01-31 00:54:44 +0900

    add apt

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "installing...."
 
-sudo apt-get -y install git gitk emacs lv rlwrap tmux vim curl libcurl3 chromium-browser ibus-mozc libreadline6-dev bison sqlite3 libsqlite3-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev openssh-server libssl-dev gauche yaml-mode nkf openjdk-7-jre-headless openjdk-7-jdk scala scala-library libv8-dev tk8.5 arduino openjdk-7-jdk openjdk-7-jre eclipse g++ xchat
+sudo apt-get -y install git gitk emacs lv rlwrap tmux vim curl libcurl3 chromium-browser ibus-mozc libreadline6-dev bison sqlite3 libsqlite3-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev openssh-server libssl-dev gauche yaml-mode nkf openjdk-7-jre-headless openjdk-7-jdk scala scala-library libv8-dev tk8.5 arduino openjdk-7-jdk openjdk-7-jre eclipse g++ xchat compizconfig-settings-manager
 
 gconftool-2 -s "/apps/metacity/general/button_layout" -t string "menu:minimize,maximize,close"
 
