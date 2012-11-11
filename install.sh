0df659719d28188ef286d3bca9b8f6edb856821f
masarakki <masaki@hisme.net>
2012-11-12 04:54:27 +0900

    fix lines

--- install.sh
+++ install.sh
@@ -2,9 +2,47 @@
 
 echo "installing...."
 
-sudo apt-get -y install git gitk emacs lv rlwrap tmux vim curl libcurl3 chromium-browser ibus-mozc libreadline6-dev bison sqlite3 libsqlite3-dev libxml2 libxml2-dev libxslt1.1 libxslt1-dev openssh-server libssl-dev gauche yaml-mode nkf openjdk-7-jre-headless openjdk-7-jdk scala scala-library libv8-dev tk8.5 arduino openjdk-7-jdk openjdk-7-jre eclipse g++ xchat compizconfig-settings-manager convmv coffeescript libcurl4-openssl-dev libdbus-glib-1-dev mysql-server libmysql++-dev xsel libsasl2-dev imagemagick libmagickcore-dev ffmpeg libavcodec-extra-53 redis-server colordiff zsh
+sudo apt-get -y install\
+ git gitk\
+ zsh\
+ autoconf2.13\
+ lv\
+ rlwrap\
+ tmux\
+ vim emacs\
+ curl libcurl3 libcurl4-openssl-dev\
+ chromium-browser\
+ ibus-mozc\
+ libreadline6-dev\
+ bison\
+ sqlite3 libsqlite3-dev\
+ libxml2 libxml2-dev\
+ libxslt1.1 libxslt1-dev\
+ openssh-server libssl-dev\
+ yaml-mode\
+ nkf\
+ openjdk-7-jre openjdk-7-jre-headless openjdk-7-jdk\
+ scala scala-library\
+ libv8-dev\
+ tk8.5\
+ arduino\
+ eclipse\
+ g++\
+ gauche\
+ coffeescript\
+ xchat\
+ compizconfig-settings-manager\
+ convmv\
+ libdbus-glib-1-dev\
+ mysql-server libmysql++-dev\
+ xsel\
+ libsasl2-dev\
+ imagemagick libmagickcore-dev\
+ ffmpeg libavcodec-extra-53\
+ redis-server\
+ colordiff
 
-gconftool-2 -s "/apps/metacity/general/button_layout" -t string "menu:minimize,maximize,close"
+#gconftool-2 -s "/apps/metacity/general/button_layout" -t string "menu:minimize,maximize,close"
 
 if [ ! -e $HOME/.rvm ] ; then
     echo "install ruby..."
@@ -23,5 +61,5 @@ if [ ! -e $HOME/.oh-my-zsh ] ; then
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 cp `dirname $0`/Ricty-* $HOME/.fonts
-fc-cache
+#fc-cache
 sh `dirname $0`/update.sh
