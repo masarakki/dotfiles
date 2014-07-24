dadc1f23a461aa799c1c713442ee8711eb1dcabf
masarakki <masaki@hisme.net>
2014-07-25 07:40:39 +0900

    update color

--- _install_ubuntu.sh
+++ _install_ubuntu.sh
@@ -79,7 +79,8 @@ gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_custom_command --typ
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/custom_command --type string "tmux"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/default_size_rows --type int 28
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/default_size_columns --type int 108
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "#000000000000:#AAAA00000000:#0000AAAA0000:#AAAA55550000:#00000000AAAA:#AAAA0000AAAA:#0000AAAAAAAA:#AAAAAAAAAAAA:#555555555555:#FFFF55555555:#5555FFFF5555:#FFFFFFFF5555:#55555555FFFF:#FFFF5555FFFF:#5555FFFFFFFF:#FFFFFFFFFFFF"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette --type string "
+#000000000000:#AAAA00000000:#0000AAAA0000:#AAAA55550000:#00000000AAAA:#AAAA0000AAAA:#0000AAAAAAAA:#AAAAAAAAAAAA:#555555555555:#FFFF55555555:#5555FFFF5555:#FFFFFFFF5555:#55555555FFFF:#FFFF5555FFFF:#5555FFFFFFFF:#FFFFFFFFFFFF"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "#FFFFFFFFFFFF"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_theme_colors --type bool false
 
