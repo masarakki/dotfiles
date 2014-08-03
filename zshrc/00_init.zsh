acb06f200e54c53df83f4bdd4ad3e6baf90de91b
masarakki <masaki@hisme.net>
2014-08-03 21:40:33 +0900

    fix PATH setup

---
+++ zshrc/00_init.zsh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -2,3 +2,7 @@
 
 export EDITOR='/usr/bin/vim'
 export PAGER='/usr/bin/lv -c'
+export PATH=$HOME/bin:$HOME/dotfiles/bin:$PATH
+export PATH=/usr/local/heroku/bin:$PATH
+
+source ~/.rvm/scripts/rvm
