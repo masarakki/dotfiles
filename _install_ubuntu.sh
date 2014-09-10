8d8810a817fcf9bc9a14b0513887723ee3eb64d0
masarakki <masaki@hisme.net>
2014-09-11 06:25:38 +0900

    fix oh-my-zsh installation

--- _install_ubuntu.sh
+++ _install_ubuntu.sh
@@ -74,6 +74,8 @@ fi
 
 if [ ! -e $HOME/.oh-my-zsh ] ; then
     curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+    chsh -s /bin/zsh
+    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="philips"/' ~/.zshrc
 fi
 cp `dirname $0`/Ricty-* $HOME/.fonts
 #fc-cache
