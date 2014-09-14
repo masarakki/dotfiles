0d2ac6ce8626decccde9bf069a2535642ff0a326
masarakki <masaki@hisme.net>
2014-09-14 22:06:16 +0900

    change fontsize depends on display size

--- _install_ubuntu.sh
+++ _install_ubuntu.sh
@@ -79,8 +79,16 @@ if [ ! -e $HOME/.oh-my-zsh ] ; then
 fi
 cp `dirname $0`/Ricty-* $HOME/.fonts
 #fc-cache
+
+if [ `xrandr | grep current | sed -E 's/.*current ([0-9]+).*/\1/'` -gt 2048 ]
+then
+    fontsize=16
+else
+    fontsize=14
+fi
+
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type bool false
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ricty Regular 14"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ricty Regular ${fontsize}"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_custom_command --type bool true
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/custom_command --type string "tmux"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/default_size_rows --type int 28
