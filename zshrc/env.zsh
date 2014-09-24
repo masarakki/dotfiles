a77555daace7f997c8bb129fea8b04fa1ad511bb
masarakki <masaki@hisme.net>
2014-09-24 19:33:40 +0900

    fix checkout

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -3,7 +3,7 @@
 export EDITOR='/usr/bin/vim'
 export PAGER='/usr/bin/lv -c'
 export GOPATH=$HOME
-export PATH=$HOME/bin:`pwd`/bin:$PATH
+export PATH=$HOME/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$PATH
 export PATH=/usr/local/heroku/bin:$PATH
 
 source ~/.rvm/scripts/rvm
