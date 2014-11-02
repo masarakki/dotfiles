61d091477ee3061aec8e7936ef5429640753f773
masarakki <masaki@hisme.net>
2014-11-03 05:05:15 +0900

    set N_PREFIX

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -5,5 +5,6 @@ export PAGER='/usr/bin/lv -c'
 export GOPATH=$HOME
 export PATH=$HOME/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$PATH
 export PATH=/usr/local/heroku/bin:$PATH
+export N_PREFIX=$HOME
 
 source ~/.rvm/scripts/rvm
