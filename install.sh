4183b5ab1a98b3b18c60f6d4c6d4c81d36face29
masarakki <masaki@hisme.net>
2014-11-28 01:57:12 +0900

    add gpg key

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ fi
 
 if [ ! -e $HOME/.rvm ] ; then
     echo "install ruby..."
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
     RUBY_VERSION=ruby-2.1.2
     \curl -sSL get.rvm.io | bash -s stable
     $HOME/.rvm/scripts/rvm
