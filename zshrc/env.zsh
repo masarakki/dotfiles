b770637bc41830526dbd4fdba4847d97490cd79d
masarakki <masaki@hisme.net>
2015-01-03 11:25:31 +0900

    use chefs golang

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -3,9 +3,8 @@
 export EDITOR='/usr/bin/vim'
 export PAGER='/usr/bin/lv -c'
 export GOPATH=$HOME
-export PATH=$HOME/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$PATH
+export PATH=$HOME/bin:$HOME/src/github.com/masarakki/dotfiles/bin:/usr/local/go/bin:$PATH
 export PATH=/usr/local/heroku/bin:$PATH
 export N_PREFIX=$HOME
 
 source $HOME/.rvm/scripts/rvm
-source $HOME/.gvm/scripts/gvm
