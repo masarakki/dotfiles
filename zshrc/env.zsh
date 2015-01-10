926650de1fb4f76b63f009ea534ce65e57111602
masarakki <masaki@hisme.net>
2015-01-10 18:03:03 +0900

    add GOBIN

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -2,10 +2,12 @@
 
 export EDITOR='/usr/bin/vim'
 export PAGER='/usr/bin/lv -c'
+
 export GOPATH=$HOME
 export PATH=$HOME/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$PATH
-export PATH=/usr/local/heroku/bin:$PATH
 export N_PREFIX=$HOME
 
 source $HOME/.rvm/scripts/rvm
 source $HOME/.gvm/scripts/gvm
+
+export GOBIN=$HOME/bin
