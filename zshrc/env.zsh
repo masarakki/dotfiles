9dc19d49ae06114ed8c6a973131ef0bd53baa9d0
masarakki <masaki@hisme.net>
2016-03-29 02:46:01 +0900

    fix

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -5,7 +5,7 @@ export PAGER='/usr/bin/lv -c'
 
 export GOROOT=$HOME/go
 export GOPATH=$HOME
-export GOBIN=$GOPATH/bine
+export GOBIN=$GOPATH/bin
 export N_PREFIX=$HOME/.nvm
 export PATH=$HOME/bin:$GOROOT/bin:$N_PREFIX/bin:$HOME/src/github.com/masarakki/dotfiles/bin:/usr/local/heroku/bin:$HOME/androi-studio/bin:$PATH
 
