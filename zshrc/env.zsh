d7f968d3103bd11717de4cceaf94cf813ceee664
masarakki <masaki182@gmail.com>
2017-07-28 17:40:33 +0900

    update

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -7,7 +7,7 @@ export GOROOT=$HOME/go
 export GOPATH=$HOME
 export GOBIN=$GOPATH/bin
 export N_PREFIX=$HOME/.nvm
-export PATH=$HOME/bin:$GOROOT/bin:$N_PREFIX/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$HOME/androi-studio/bin:$HOME/.tfenv/bin:$HOME/.local/bin:$PATH
+export PATH=$HOME/bin:$GOROOT/bin:$N_PREFIX/bin:$HOME/src/github.com/masarakki/dotfiles/bin:$HOME/.tfenv/bin:$HOME/.local/bin:$PATH
 
 source $HOME/.cargo/env
 source $HOME/.rvm/scripts/rvm
