7a1448033322510d3dd20067ee250f61400bdea6
masarakki <masaki-yamada@cookpad.jp>
2019-07-11 22:07:08 +0900

    go-to-gem

--- zshrc/peco-src.zsh
+++ zshrc/peco-src.zsh
@@ -8,5 +8,17 @@ function peco-src () {
     fi
     zle clear-screen
 }
+
+function gem-src () {
+    local selected_dir=$(( ls $GEM_HOME/gems ) | peco --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${GEM_HOME}/gems/${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+
 zle -N peco-src
 bindkey '^S' peco-src
+zle -N gem-src
+bindkey '^V' gem-src
