05d70fc52ea177773ef26aff0b02eb96dadd926e
masarakki <masaki182@gmail.com>
2019-08-28 04:21:21 +0900

    add-snap/bin

--- zshrc/00_init.zsh
+++ zshrc/00_init.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 setxkbmap -option ctrl:nocaps
