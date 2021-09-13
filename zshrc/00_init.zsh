da6d20587efb3c6b4ea3ba3fb51cc14cd82f8a24
masarakki <masaki182@gmail.com>
2021-09-13 14:23:35 +0900

    fix-compare-string

--- zshrc/00_init.zsh
+++ zshrc/00_init.zsh
@@ -4,6 +4,6 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/ga
 setxkbmap -option ctrl:nocaps
 
 logi_id=`xinput list | grep 'Logitech MX Vertical' | grep pointer | cut -f 2 | cut -f 2 -d '='`
-if [ $logi_id -gt 0 ]; then
+if [ -n "${logi_id}" ]; then
     xinput set-button-map $logi_id {1..7} 2 {9..20}
 fi
